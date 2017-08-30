58a8c826f02a65234fd35e77596113c092efdd59
Sergey-Akmalov <akmolgt@gmail.com>
2017-08-30 22:57:00 +0300

    learn updating data

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('59a70e75897d6a6343b04611')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('User').findOneAndUpdate({
+    _id: new ObjectID('59a71016897d6a6343b04647')
+  }, {
+    $set: {
+      name: 'Donald'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  db.close();
+});
